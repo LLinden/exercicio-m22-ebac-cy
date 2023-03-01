e79fff1563f4a55625ff289a1db9590c9edfeceb
Lucas Linden <lucas.linden@cwi.com.br>
2023-03-01 13:37:48 -0300

    first commit

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -2,14 +2,8 @@
 let data;
 
 describe("Checkout", () => {
-  beforeEach(() => {
-    cy.fixture("dados.json").then((p) => {
-      data = p;
-      cy.visit('/');
-    });
-  });
-
   it("deve realizar checkout com sucesso", () => {
-    //
+    cy.adicionaProduto();
+    cy.checkout();
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,18 @@
+Cypress.Commands.add("adicionaProduto", () => {
+  const fd = new FormData();
+  fd.append("product_sku", "");
+  fd.append("product_id", "8787");
+  fd.append("quantity", "1");
+
+  cy.request({
+    url: "/?wc-ajax=add_to_cart",
+    method: "POST",
+    body: fd,
+  }).then((resp) => {
+    expect(resp.status).to.eq(200);
+  });
+});
+
 Cypress.Commands.add("checkout", () => {
   const fd = new FormData();
   fd.append("billing_first_name", "Teste");
@@ -19,12 +34,11 @@ Cypress.Commands.add("checkout", () => {
   fd.append("woocommerce-process-checkout-nonce", "b9a698393b");
   fd.append("_wp_http_referer", "/?wc-ajax=update_order_review");
 
-
   cy.request({
     url: "/?wc-ajax=checkout",
     method: "POST",
     body: fd,
+  }).then((resp) => {
+    expect(resp.status).to.eq(200);
   });
-
-  cy.visit("/wp-admin");
 });
