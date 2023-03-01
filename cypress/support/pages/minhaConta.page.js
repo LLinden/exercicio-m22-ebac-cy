b20f98baed91e4d79e6dddd4145d434be6e61808
Lucas Linden <lucas.linden@cwi.com.br>
2023-03-01 11:44:59 -0300

    first commit

--- cypress/support/pages/minhaConta.page.js
+++ cypress/support/pages/minhaConta.page.js
@@ -5,7 +5,25 @@ class MinhaConta {
     cy.get("#reg_email").type(email);
     cy.get("#reg_password").type(senha, { log: false });
     cy.get(".button").contains("Register").click();
-    cy.get('.page-title').contains("Minha conta").should("be.visible");
+  }
+
+  preencheEmail(email) {
+    cy.get("#reg_email").type(email);
+    cy.get(".button").contains("Register").click();
+  }
+
+  verificaSucesso() {
+    cy.get(".woocommerce-MyAccount-navigation-link--dashboard > a").should(
+      "be.visible"
+    );
+  }
+
+  verificaFalha() {
+    cy.get(".button").contains("Register").should("be.visible");
+  }
+
+  mensagemDeErro() {
+    cy.get(".woocommerce-error").should("be.visible");
   }
 }
 export default new MinhaConta();
