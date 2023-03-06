1f3aa8bb0bf9efc2ebbc3ffe061679db2cb25343
Lucas Linden <lucas.linden@cwi.com.br>
2023-03-06 14:46:35 -0300

    exercicios m23

---
+++ cypress/e2e/carrinho.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Operações no carrinho', () => {
+    it('deve adicionar item no carrinho com sucesso', () => {
+        
+    });
+
+    it('deve remover item do carrinho com sucesso', () => {
+        
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,32 +13,35 @@ Cypress.Commands.add("adicionaProduto", () => {
   });
 });
 
-Cypress.Commands.add("checkout", () => {
-  const fd = new FormData();
-  fd.append("billing_first_name", "Teste");
-  fd.append("billing_last_name", "Teste");
-  fd.append("billing_company", "");
-  fd.append("billing_country", "BR");
-  fd.append("billing_address_1", "Rua+Teste");
-  fd.append("billing_address_2", "");
-  fd.append("billing_city", "São+Paulo");
-  fd.append("billing_state", "SP");
-  fd.append("billing_postcode", "95600-000");
-  fd.append("billing_phone", "99999999");
-  fd.append("billing_email", "testeqa@mailinator.com");
-  fd.append("account_password", "");
-  fd.append("order_comments", "");
-  fd.append("payment_method", "cod");
-  fd.append("terms", "on");
-  fd.append("terms-field", "1");
-  fd.append("woocommerce-process-checkout-nonce", "b9a698393b");
-  fd.append("_wp_http_referer", "/?wc-ajax=update_order_review");
+Cypress.Commands.add(
+  "checkout",
+  (nome, sobrenome, pais, rua, cidade, estado, cep, fone, email) => {
+    const fd = new FormData();
+    fd.append("billing_first_name", nome);
+    fd.append("billing_last_name", sobrenome);
+    fd.append("billing_company", "");
+    fd.append("billing_country", pais);
+    fd.append("billing_address_1", rua);
+    fd.append("billing_address_2", "");
+    fd.append("billing_city", cidade);
+    fd.append("billing_state", estado);
+    fd.append("billing_postcode", cep);
+    fd.append("billing_phone", fone);
+    fd.append("billing_email", email);
+    fd.append("account_password", "");
+    fd.append("order_comments", "");
+    fd.append("payment_method", "cod");
+    fd.append("terms", "on");
+    fd.append("terms-field", "1");
+    fd.append("woocommerce-process-checkout-nonce", "b9a698393b");
+    fd.append("_wp_http_referer", "/?wc-ajax=update_order_review");
 
-  cy.request({
-    url: "/?wc-ajax=checkout",
-    method: "POST",
-    body: fd,
-  }).then((resp) => {
-    expect(resp.status).to.eq(200);
-  });
-});
+    cy.request({
+      url: "/?wc-ajax=checkout",
+      method: "POST",
+      body: fd,
+    }).then((resp) => {
+      expect(resp.status).to.eq(200);
+    });
+  }
+);
