37781ac016259ac1cd3fa18fca2496017e0f7c94
Lucas Linden <lucas.linden@cwi.com.br>
2023-04-24 09:23:32 -0300

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     reporterOptions: {
       reportDir: "cypress/results",
       overwrite: false,
-      html: false,
+      html: true,
       json: true,
     },
     baseUrl: "http://lojaebac.ebaconline.art.br",
